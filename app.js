b343a6828f351483c1c8b3fad7dd33ce9ead0e0a
Kyle Hillman <khillman84@gmail.com>
2017-01-18 14:04:39 -0800

    added the round counter

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var objects = [];
 var divOne = document.getElementById('image-one');
 var divTwo = document.getElementById('image-two');
 var divThree = document.getElementById('image-three');
+var rounds = 0;
+var leftProduct;
+var centerProduct;
+var rightProduct;
 
 // object constructor
 function Items (name, path, timesShown, timesClicked){
@@ -25,7 +29,7 @@ function displayPics() {  //display three images to index.html
   var left = new Image();
   left.setAttribute('id', 'pictureOne');
   var leftIndex = randomNumber();
-  var leftProduct = objects[leftIndex];
+  leftProduct = objects[leftIndex];
   left.src = leftProduct.path;
   left.alt = leftProduct.name;
   leftProduct.timesShown += 1;
@@ -38,7 +42,7 @@ function displayPics() {  //display three images to index.html
 
   var center = new Image();
   center.setAttribute('id', 'pictureTwo');
-  var centerProduct = objects[centerIndex];
+  centerProduct = objects[centerIndex];
   center.src = centerProduct.path;
   center.alt = centerProduct.name;
   centerProduct.timesShown += 1;
@@ -51,7 +55,7 @@ function displayPics() {  //display three images to index.html
 
   var right = new Image();
   right.setAttribute('id', 'pictureThree');
-  var rightProduct = objects[rightIndex];
+  rightProduct = objects[rightIndex];
   right.src = rightProduct.path;
   right.alt = rightProduct.name;
   rightProduct.timesShown += 1;
@@ -72,64 +76,51 @@ function groupFunctions() {
   console.log(objects);
   var elRefreshOne = document.getElementById('pictureOne');
   elRefreshOne.addEventListener('click', function() {
+    leftProduct.timesClicked++;
     removePics();
     groupFunctions();
   }, false);
   var elRefreshTwo = document.getElementById('pictureTwo');
   elRefreshTwo.addEventListener('click', function() {
+    centerProduct.timesClicked++;
     removePics();
     groupFunctions();
   }, false);
   var elRefreshThree = document.getElementById('pictureThree');
   elRefreshThree.addEventListener('click', function() {
+    rightProduct.timesClicked++;
     removePics();
     groupFunctions();
   }, false);
+  rounds++;
+  if (rounds < 6) {
+    console.log('Round number: ' + rounds);
+  } else {
+    final();
+  }
 }
 
 // objects
-var bag = new Items ('bag', 'img/bag.jpg', 0, 0);
-objects.push(bag);
-var banana = new Items ('banana', 'img/banana.jpg', 0, 0);
-objects.push(banana);
-var bathroom = new Items ('bathroom', 'img/bathroom.jpg', 0, 0);
-objects.push(bathroom);
-var boots = new Items ('boots', 'img/boots.jpg', 0, 0);
-objects.push(boots);
-var breakfast = new Items ('breakfast', 'img/breakfast.jpg', 0, 0);
-objects.push(breakfast);
-var bubblegum = new Items ('bubblegum', 'img/bubblegum.jpg', 0, 0);
-objects.push(bubblegum);
-var chair = new Items ('chair', 'img/chair.jpg', 0, 0);
-objects.push(chair);
-var cthulhu = new Items ('cthulhu', 'img/cthulhu.jpg', 0, 0);
-objects.push(cthulhu);
-var dogDuck = new Items ('dog-duck', 'img/dog-duck.jpg', 0, 0);
-objects.push(dogDuck);
-var dragon = new Items ('dragon', 'img/dragon.jpg', 0, 0);
-objects.push(dragon);
-var pen = new Items ('pen', 'img/pen.jpg', 0, 0);
-objects.push(pen);
-var petSweep = new Items ('pet-sweep', 'img/pet-sweep.jpg', 0, 0);
-objects.push(petSweep);
-var scissors = new Items ('scissors', 'img/scissors.jpg', 0, 0);
-objects.push(scissors);
-var shark = new Items ('shark', 'img/shark.jpg', 0, 0);
-objects.push(shark);
-var sweep = new Items ('sweep', 'img/sweep.png', 0, 0);
-objects.push(sweep);
-var tauntaun = new Items ('tauntaun', 'img/tauntaun.jpg', 0, 0);
-objects.push(tauntaun);
-var unicorn = new Items ('unicorn', 'img/unicorn.jpg', 0, 0);
-objects.push(unicorn);
-var usb = new Items ('usb', 'img/usb.gif', 0, 0);
-objects.push(usb);
-var waterCan = new Items ('water-can', 'img/water-can.jpg', 0, 0);
-objects.push(waterCan);
-var wineGlass = new Items ('wine-glass', 'img/wine-glass.jpg', 0, 0);
-objects.push(wineGlass);
+objects.push(new Items ('bag', 'img/bag.jpg', 0, 0));
+objects.push(new Items ('banana', 'img/banana.jpg', 0, 0));
+objects.push(new Items ('bathroom', 'img/bathroom.jpg', 0, 0));
+objects.push(new Items ('boots', 'img/boots.jpg', 0, 0));
+objects.push(new Items ('breakfast', 'img/breakfast.jpg', 0, 0));
+objects.push(new Items ('bubblegum', 'img/bubblegum.jpg', 0, 0));
+objects.push(new Items ('chair', 'img/chair.jpg', 0, 0));
+objects.push(new Items ('cthulhu', 'img/cthulhu.jpg', 0, 0));
+objects.push(new Items ('dog-duck', 'img/dog-duck.jpg', 0, 0));
+objects.push(new Items ('dragon', 'img/dragon.jpg', 0, 0));
+objects.push(new Items ('pen', 'img/pen.jpg', 0, 0));
+objects.push(new Items ('pet-sweep', 'img/pet-sweep.jpg', 0, 0));
+objects.push(new Items ('scissors', 'img/scissors.jpg', 0, 0));
+objects.push(new Items ('shark', 'img/shark.jpg', 0, 0));
+objects.push(new Items ('sweep', 'img/sweep.png', 0, 0));
+objects.push(new Items ('tauntaun', 'img/tauntaun.jpg', 0, 0));
+objects.push(new Items ('unicorn', 'img/unicorn.jpg', 0, 0));
+objects.push(new Items ('usb', 'img/usb.gif', 0, 0));
+objects.push(new Items ('water-can', 'img/water-can.jpg', 0, 0));
+objects.push(new Items ('wine-glass', 'img/wine-glass.jpg', 0, 0));
 
 // run functions
 groupFunctions();
-
-// event listeners
